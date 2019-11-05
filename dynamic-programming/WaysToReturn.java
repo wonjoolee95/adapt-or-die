25f3d9a47a82040976b8943bccfebd49ad9b0f80
Wonjoo Lee <jeromelee95@gmail.com>
2019-11-04 16:36:02 -0800

    Update WaysToReturn.java

--- dynamic-programming/WaysToReturn.java
+++ dynamic-programming/WaysToReturn.java
@@ -14,28 +14,27 @@ Exaplanation:
 */
 
 class Solution {
-
-	/*
-	 * This is the brute force recursive appoach. At each index, we take every possible path.
-	 * That is -- left, stay, and right. If we have 0 moves left and are at index 0, we know
-	 * we have found a path. 
-	 * 
-	 * Time: O(3^n), where n is the number of moves, as we branch out to 3 branches up to n.
-	 * Space: O(n), as recursive call stack goes down to n.
-	 */
+    /*
+     * This is the brute force recursive appoach. At each index, we take every possible path.
+     * That is -- left, stay, and right. If we have 0 moves left and are at index 0, we know
+     * we have found a path. 
+     * 
+     * Time: O(3^n), where n is the number of moves, as we branch out to 3 branches up to n.
+     * Space: O(n), as recursive call stack goes down to n.
+     */
     public int waysToReturn(int length, int moves) {
-    	return helper(length, moves, 0);
+        return helper(length, moves, 0);
     }
-    
+
     private int helper(int length, int moves, int index) {
         if (moves == 0 && index == 0) {
             return 1;
         } else if (moves == 0) {
             return 0;
         } else if (index == length || index < 0) {
-        	return 0;
+            return 0;
         }
-        
+
         int left = helper(length, moves - 1, index - 1);
         int stay = helper(length, moves - 1, index);
         int right = helper(length, moves - 1, index + 1);
@@ -44,37 +43,37 @@ class Solution {
         return ways;
     }
 
-	/*
-	 * This is memoized top-down dynamic programming approach. We keep a Map<Integer, Map<Integer, Integer>>,
-	 * mapping {moves: {index: waysToReturn}}. For each move, if at that index, we have calculated the
-	 * ways to return, we store it in our memo. 
+    /*
+     * This is memoized top-down dynamic programming approach. We keep a Map<Integer, Map<Integer, Integer>>,
+     * mapping {moves: {index: waysToReturn}}. For each move, if at that index, we have calculated the
+     * ways to return, we store it in our memo. 
      *
-	 * Time: O(n^2), where n is the number of moves. Time complexity of these top-down memozied recursive
-	 * algorithms are tricky to figure out. In these algorithms, instead of looking at the code, look at 
-	 * the bigger algorithmic picture. In our code, we are looking at every path once, since we use memo.
-	 * At each recursive stack, we move or stay at a spot. That means at
-	 * each index, we'll recursing down to n - 1 paths. This is similar to a typical O(n^2) double-nested
-	 * algorithm. 
-	 * Space: O(n^2), as we are storing ways for every path. 
-	 */
+     * Time: O(n^2), where n is the number of moves. Time complexity of these top-down memozied recursive
+     * algorithms are tricky to figure out. In these algorithms, instead of looking at the code, look at 
+     * the bigger algorithmic picture. In our code, we are looking at every path once, since we use memo.
+     * At each recursive stack, we move or stay at a spot. That means at
+     * each index, we'll recursing down to n - 1 paths. This is similar to a typical O(n^2) double-nested
+     * algorithm. 
+     * Space: O(n^2), as we are storing ways for every path. 
+     */
     public int waysToReturn(int length, int moves) {
-    	Map<Integer, Map<Integer, Integer>> memo = new HashMap<>();
-    	return helper(length, moves, 0);
+        Map < Integer, Map < Integer, Integer >> memo = new HashMap < > ();
+        return helper(length, moves, 0);
     }
-    
-    private int helper(int length, int moves, int index, Map<Integer, Map<Integer, Integer>> memo) {
-		if (memo.containsKey(moves) && memo.get(moves).containsKey(index)) {
-			return memo.get(moves).get(index);
-		}
+
+    private int helper(int length, int moves, int index, Map < Integer, Map < Integer, Integer >> memo) {
+        if (memo.containsKey(moves) && memo.get(moves).containsKey(index)) {
+            return memo.get(moves).get(index);
+        }
 
         if (moves == 0 && index == 0) {
             return 1;
         } else if (moves == 0) {
             return 0;
         } else if (index == length || index < 0) {
-        	return 0;
+            return 0;
         }
-        
+
         int left = helper(length, moves - 1, index - 1);
         int stay = helper(length, moves - 1, index);
         int right = helper(length, moves - 1, index + 1);
@@ -82,47 +81,47 @@ class Solution {
         int ways = left + stay + right;
 
         if (!memo.containsKey(moves)) {
-        	memo.put(moves, new HashMap<>());
+            memo.put(moves, new HashMap < > ());
         }
 
         memo.get(moves).put(index, ways);
 
         return ways;
     }
-    
-  /*
-   * This is bottom-up dynamic programming approach. Transforming the recursive brute force approach
-   * to the top-down memoized recursive approach is straight forward. Transforming the top-down 
-   * memozied approach to bottom-up dynamic programming may be tricky sometimes. When this is tricky,
-   * define a clear recurrence function. 
-   *
-   * Recurrence
-   * Let T(m, i) be the n umber of ways to reach index 0 with m moves at index i.
-   * T(m, i) = T(m - 1, i - 1) + T(m - 1, i) + T(m - 1, i + 1).
-   *
-   * We define a dp[moves + 1][length]. The rows indicate moves and columns indicate the index.
-   * Each index dp[i][j] represents the number of ways to reach index 0 with i moves at index j.
-   *
-   * Time: O(n^2), where n is the number of moves. 
-   * Space: O(n^2).
-   */
-    public int waysToReturn(int length, int moves) { 
-    	// Recurrence
-    	// Let T(m, i) be the number of ways to reach index 0 with m moves at index i.
-    	// T(m, i) = T(m - 1, i - 1) + T(m - 1, i) + T(m - 1, i + 1).
-    	int[] dp = new int[moves + 1][length];
-    	dp[0][0] = 1;
 
-    	for (int i = 1; i <= moves; i++) {
-    		for (int j = 0; j < moves; j++) {
-    			dp[i][j] += dpi[i - 1][j];
-    			if (j - 1 >= 0) dp[i][j] += dp[i - 1][j - 1];
-    			if (j + 1 < length) dp[i][j] += dp[i - 1][j + 1];
-    		}
-    	}
+    /*
+     * This is bottom-up dynamic programming approach. Transforming the recursive brute force approach
+     * to the top-down memoized recursive approach is straight forward. Transforming the top-down 
+     * memozied approach to bottom-up dynamic programming may be tricky sometimes. When this is tricky,
+     * define a clear recurrence function. 
+     *
+     * Recurrence
+     * Let T(m, i) be the n umber of ways to reach index 0 with m moves at index i.
+     * T(m, i) = T(m - 1, i - 1) + T(m - 1, i) + T(m - 1, i + 1).
+     *
+     * We define a dp[moves + 1][length]. The rows indicate moves and columns indicate the index.
+     * Each index dp[i][j] represents the number of ways to reach index 0 with i moves at index j.
+     *
+     * Time: O(n^2), where n is the number of moves. 
+     * Space: O(n^2).
+     */
+    public int waysToReturn(int length, int moves) {
+        // Recurrence
+        // Let T(m, i) be the number of ways to reach index 0 with m moves at index i.
+        // T(m, i) = T(m - 1, i - 1) + T(m - 1, i) + T(m - 1, i + 1).
+        int[] dp = new int[moves + 1][length];
+        dp[0][0] = 1;
 
-    	return dp[moves][0];
-	}
+        for (int i = 1; i <= moves; i++) {
+            for (int j = 0; j < moves; j++) {
+                dp[i][j] += dpi[i - 1][j];
+                if (j - 1 >= 0) dp[i][j] += dp[i - 1][j - 1];
+                if (j + 1 < length) dp[i][j] += dp[i - 1][j + 1];
+            }
+        }
+
+        return dp[moves][0];
+    }
 }
 
 /*
